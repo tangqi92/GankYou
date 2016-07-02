eb668e7f7c464964b8bf0ba3d3b8c1e0a4da0f13
tangqi92 <imtangqi@gmail.com>
2016-07-02 09:23:42 +0800

    Initial Commit

---
+++ GankYou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GankYou
+//
+//  Created by Tang Qi on 7/2/16.
+//  Copyright © 2016 Tang Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GankYou/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GankYou
+//
+//  Created by Tang Qi on 7/2/16.
+//  Copyright © 2016 Tang Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
